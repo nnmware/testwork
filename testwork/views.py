2005fbbf58bac484502e9dfece87cedd1be753d0
nnmware <nnmware@gmail.com>
2013-02-04 14:41:36 +1100

    url fix

--- testwork/views.py
+++ testwork/views.py
@@ -10,6 +10,6 @@ def manage_json(request, model_name=None):
         model = cache.app_models['Dynamic'][model_name]
         result = serializers.serialize("json", model.objects.all())
     else:
-        payload = {'success': True, 'models': cache.app_models['Dynamic'].keys()}
+        payload = {'models': cache.app_models['Dynamic'].keys()}
         result = simplejson.dumps(payload)
     return HttpResponse(result, content_type='application/json')
